c5e6dcabf1f34006f4060cc384c873fba43d5157
RainerJamil <colqmam2112@gmail.com>
2024-04-05 11:44:33 -0400

    qw

--- src/practica4/ejercicio4vector.java
+++ src/practica4/ejercicio4vector.java
@@ -1,7 +1,14 @@
 package practica4;
 
+import java.util.Scanner;
+
 public class ejercicio4vector {
     public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+        System.out.println("NUMERO DE ESTUDIANTES");
+        int A= leer.nextInt();
+        int []P=new int[A];
+        String[]N=new String[A];
 
     }
 }
